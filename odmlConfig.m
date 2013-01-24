b314128c670f1c85ee40c5f07a9b14245876271d
Jan Grewe <jan.grewe@g-node.org>
2013-01-24 17:43:06 +0100

    update of the used library, according changes to the odmlConfig, new odml.jar

--- odmlConfig.m
+++ odmlConfig.m
@@ -17,12 +17,16 @@ workDir = path(1:indices(end));
 
 warning('off', 'all');
 javarmpath([workDir 'odml.jar']);
-javarmpath([workDir 'log4j-1.2.15.jar']);
+javarmpath([workDir 'slf4j-jdk14.jar']);
+javarmpath([workDir 'slf4j-api.jar']);
+javarmpath([workDir 'commons-codec-1.5.jar']);
 % javarmpath([workDir 'jdom.jar']); % uncomment this line for older
 %versions of Matlab that do not include jdom.jar
 
 warning('on', 'all');
 javaaddpath([workDir 'odml.jar']);
-javaaddpath([workDir 'log4j-1.2.15.jar']);
+javaaddpath([workDir 'slf4j-jdk14.jar']);
+javaaddpath([workDir 'slf4j-api.jar']);
+javaaddpath([workDir 'commons-codec-1.5.jar']);
 % javaaddpath([workDir 'jdom.jar']); %uncomment this line for older
 %    matlab versions that do not have jdom by default...
