be71527783fd499aa5ce2b3b7bfc2636a3f31456
Jan Grewe <jan.grewe@g-node.org>
2015-01-13 22:50:12 +0100

    change odmlConfig to work with the new lib and dependencies

--- odmlConfig.m
+++ odmlConfig.m
@@ -12,21 +12,15 @@ function odmlConfig()
 % for more information visit: www.g-node.org/odml
 
 path = mfilename('fullpath');
-indices = findstr(path,filesep);
+indices = strfind(path,filesep);
 workDir = path(1:indices(end));
 
 warning('off', 'all');
 javarmpath([workDir 'odml.jar']);
-javarmpath([workDir 'slf4j-jdk14.jar']);
-javarmpath([workDir 'slf4j-api.jar']);
 javarmpath([workDir 'commons-codec-1.5.jar']);
-% javarmpath([workDir 'jdom.jar']); % uncomment this line for older
-%versions of Matlab that do not include jdom.jar
+javarmpath([workDir 'jdom-2.0.5.jar']);
 
 warning('on', 'all');
 javaaddpath([workDir 'odml.jar']);
-javaaddpath([workDir 'slf4j-jdk14.jar']);
-javaaddpath([workDir 'slf4j-api.jar']);
 javaaddpath([workDir 'commons-codec-1.5.jar']);
-% javaaddpath([workDir 'jdom.jar']); %uncomment this line for older
-%    matlab versions that do not have jdom by default...
+javaaddpath([workDir 'jdom-2.0.5.jar']);
